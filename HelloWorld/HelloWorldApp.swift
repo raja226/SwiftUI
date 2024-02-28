d4eabfd388a501e72cfd53d89728bece5aa36ed9
raja226-kanini <144207791+raja226-kanini@users.noreply.github.com>
2024-02-28 05:42:14 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Administrator on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
