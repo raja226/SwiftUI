6a5e77a8230b0c710cdc6b37589b3719b6e5bdcb
raja226-kanini <144207791+raja226-kanini@users.noreply.github.com>
2024-02-28 16:07:52 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Administrator on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
