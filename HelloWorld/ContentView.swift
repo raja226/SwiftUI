bc58e8522b5bbecf0dc293dc93c4cb2bfef5e9d5
raja226-kanini <144207791+raja226-kanini@users.noreply.github.com>
2024-02-28 15:51:08 +0530

    initial hellow word with gradient color

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let imageURL:String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        // MARK: - 1 Basic
+       // AsyncImage(url: URL(string: imageURL))
+        // MARK: - 2. scale
+        //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        // MARK: - 3. placeholder
+        
+
+        // Gradient color :
+        Text("iOS")
+            .font(.system(size: 180))
+            .foregroundStyle(
+                LinearGradient(colors: [.pink,.purple,.blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+            ).padding()
+
+        
+        
     }
 }
 
